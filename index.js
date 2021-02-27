10ff63e70a05b05d9db1c0c4c500dc8db1cd2901
Asel <kulbachaevaasel@gmail.com>
2021-02-27 19:23:01 +0600

    Add documentation

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 console.log(1);
 console.log(2);
+
+
+// git -init
+// git add index.js
+// git commit -m "Рассылка почты"
+// git add index.js                                      // илменяем документации
+// git commt -m "Добавлено рассылка для телеги"             
